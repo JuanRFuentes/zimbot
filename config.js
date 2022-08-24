bbccb178fdf00fcf7ce99715984fb2f644b3c8cd
Juan <90003192+JuanRFuentes@users.noreply.github.com>
2022-08-24 16:17:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -37,8 +37,8 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
+global.owner = ['523222383026','']
+global.premium = ['523221383036','523221383036']
 global.botname = 'ZIM BOT INC'
 global.ownername = 'DRIPS'
 global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
